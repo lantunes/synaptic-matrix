6a804aa06535f92b4fe870760df83f546162bee4
Luis Antunes <lantunes@guidewire.com>
2016-12-25 10:58:35 -0500

    setting the neuron activation value to 1

--- src/main/java/com/algoritica/neurons/Neuron.java
+++ src/main/java/com/algoritica/neurons/Neuron.java
@@ -53,21 +53,37 @@ public class Neuron extends ConcurrentCognitiveComponent {
 
         ActionPotential input = incoming.receive();
 
-        //subtract leak amount since last input
+        /* subtract leak amount since last input */
         long now = System.nanoTime();
         int leakAmount = amountLeaked(now);
         lastInputTimeNano = now;
         potential = (leakAmount > potential) ? 0 : potential - leakAmount;
 
-        //integrate input
+        /* integrate input */
         potential += input.get();
         System.out.println("[neuron " + id + "][" + System.nanoTime() + " ns][INPUT]: " + input +
                 " {potential: " + potential + ", leaked: " + leakAmount + "}");
 
         if (potential > threshold) {
-            //spike
+            /* spike */
             System.out.println("[neuron " + id + "][" + System.nanoTime() + " ns][SPIKE]");
-            outgoing.send(new ActionPotential(potential));
+            /*
+             * Since 1 is always the activation value sent, the activation function is a
+             * binary step function, with a range of {0,1}.
+             *
+             * Could we replace this value with some function of the potential, such as a sigmoidal function?
+             * What would be the biological relevance of the resulting value in this context?
+             * In a traditional neural net, this value could be interpreted as the
+             * "firing rate", or "activity" of the neuron.
+             *
+             * A difference is that, in a common neural net, the activation function is always computed,
+             * regardless of the value of the summation (of inputs*weights); whereas what's being proposed here,
+             * is that the activation function is invoked only if a threshold is surpassed.
+             *
+             * With this concurrent neuronal system, it seems that, to achieve the result of sigmoidal activation in
+             * common neural nets, the neuron's spiking behaviour over a time range would have to be considered.
+             */
+            outgoing.send(new ActionPotential(1));
 
             //send a message back to incoming synapses for STDP
             //TODO this isn't going to work, as it will trigger incoming.receive() in this process
